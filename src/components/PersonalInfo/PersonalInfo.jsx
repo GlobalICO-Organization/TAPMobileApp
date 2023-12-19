c1818b27e443e58ad2f3f7d38547b18c9c3ccba4
Akash Saha Crypto Asset Rating <89798622+akash-saha-crypto@users.noreply.github.com>
2023-12-20 02:05:08 +0530

    Update PersonalInfo.jsx

--- src/components/PersonalInfo/PersonalInfo.jsx
+++ src/components/PersonalInfo/PersonalInfo.jsx
@@ -191,7 +191,7 @@ const PersonalInfo = () => {
       temp.dateOfSubmission = Date.now()
       temp.approvedByICA = false
       temp.isFromUSA = userData.country.toLowerCase() === "united states of america" ? true : false
-      temp.company = userData?.company.toLowerCase().trim();
+      temp.company = userData?.company?.toLowerCase()?.trim();
       dispatch(setUserData(temp))
       history.push(`/${userId}/capture-id-info`)
     },
